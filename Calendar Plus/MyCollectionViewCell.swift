f9558f0fc5e98411aefc57c8bfcd030cde391d89
claudiastuck <claudia.stuck21@gmail.com>
2019-05-23 14:21:02 -0400

    first

---
+++ Calendar Plus/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  Calendar Plus
+//
+//  Created by  on 5/23/19.
+//  Copyright © 2019 Claudia's Apps. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+}
--- Calendar Plus/ViewController.swift
+++ Calendar Plus/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
+                "myCell", for: indexPath) as! MyCollectionViewCell
+        cell.backgroundColor = UIColor.yellow
+        cell.myLabel.text = "Claudia"
+       // cell.imageView.image = images[indexPath.row]
+        
+        return cell
     }
 
 
